4982d6a87636c90cfd0abd48492e236f70583064
Chih Kai Wen <ckwen1988@gmail.com>
2025-05-02 20:18:44 +0800

    feat: update service repo

--- app/domain/interfaces.ts
+++ app/domain/interfaces.ts
@@ -2,7 +2,7 @@ import { UserEntity, UserId, WaterLogEntity } from './entities';
 
 export interface IUserRepository {
   create(user: UserEntity): Promise<void>;
-  findById(id: UserId): Promise<UserEntity | null>;
+  findById(id: UserId): Promise<UserEntity>;
   updateWeight(id: UserId, weight: number): Promise<void>;
   updateTargetWater(id: UserId, target: number): Promise<void>;
   getAll(): Promise<UserEntity[]>;
@@ -10,7 +10,7 @@ export interface IUserRepository {
 
 export interface IWaterLogRepository {
   create(waterlog: WaterLogEntity): Promise<number>;
-  getTodayTotal(id: UserId): Promise<number>;
+  getTodayWaterLogs(id: UserId): Promise<WaterLogEntity[]>;
 }
 
 export interface IHttpClient {
