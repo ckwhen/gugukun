0151208c3cf239b44fbba7e825c03a15da34c4cb
Chih Kai Wen <ckwen1988@gmail.com>
2025-05-02 20:19:24 +0800

    feat: add bubble Flex Message

--- app/webhook/messageHandler.ts
+++ app/webhook/messageHandler.ts
@@ -21,7 +21,9 @@ export async function handleMessage(event: MessageEvent, client: any) {
 
   const { text: messageText } = message;
   const phraseType = checkPhraseTypeByMessage(messageText);
-  let text: LinesText | LineText = getPhraseTextByType(phraseType);
+  const messages = [];
+
+  messages.push(getPhraseTextByType(phraseType));
 
   if (userId && phraseType === PHRASE_TYPES.SET_GOAL) {
     userService.handleUserSetting(userId, messageText);
@@ -30,23 +32,103 @@ export async function handleMessage(event: MessageEvent, client: any) {
   if (userId && phraseType === PHRASE_TYPES.CHECK_GOAL) {
     const user = await userService.handleUserFind(userId);
 
-    text = getPhraseTextByType(phraseType, { cc: user?.targetWater });
+    messages[0] = getPhraseTextByType(phraseType, { cc: user?.targetWater });
   }
 
   if (userId && phraseType === PHRASE_TYPES.RECORD_WATER) {
     const amount = await userService.handleWaterLogCreate(userId, messageText);
 
-    text = getPhraseTextByType(phraseType, { cc: amount });
+    messages[0] = getPhraseTextByType(phraseType, { cc: amount });
   }
 
   if (userId && phraseType === PHRASE_TYPES.CHECK_CURRENT) {
-    const total = await userService.handleTodayTotalWater(userId);
+    const progress = await userService.handleUserWaterProgress(userId);
+    const {
+      percentage,
+      targetWater,
+      totalWaterToday,
+    } = progress;
 
-    text = getPhraseTextByType(phraseType, { cc: total });
+    messages[0] = getPhraseTextByType(phraseType, { cc: totalWaterToday });
+    messages.push(
+      {
+        "type": "flex",
+        "altText": "This is a Flex Message",
+        "contents": {
+          "type": "bubble",
+          "size": "deca",
+          "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "進度",
+                "color": "#ffffff",
+                "align": "start",
+                "size": "md",
+                "gravity": "center"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "color": "#ffffff",
+                    "text": percentage
+                  },
+                  {
+                    "type": "text",
+                    "color": "#ffffff",
+                    "text": `${totalWaterToday}/${targetWater}`,
+                    "align": "end"
+                  }
+                ],
+                "margin": "lg"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "filler"
+                      }
+                    ],
+                    "width": percentage,
+                    "backgroundColor": "#0D8186",
+                    "height": "6px"
+                  }
+                ],
+                "backgroundColor": "#9FD8E36E",
+                "height": "6px",
+                "margin": "md"
+              }
+            ],
+            "backgroundColor": "#27ACB2",
+            "paddingTop": "19px",
+            "paddingAll": "12px",
+            "paddingBottom": "16px"
+          },
+          "styles": {
+            "body": {
+              "separator": false
+            },
+            "footer": {
+              "separator": false
+            }
+          }
+        }
+      }
+    );
   }
 
   if (phraseType === PHRASE_TYPES.GET_HELP) {
-    text = [
+    messages[0] = [
       '咕咕～咕咕君來提醒你摟咕',
       '只要輸入 help 我都會出現喔咕',
       '',
@@ -54,8 +136,10 @@ export async function handleMessage(event: MessageEvent, client: any) {
     ];
   }
 
+  const [ commonText, ...rest ] = messages;
+
   return client.replyMessage({
     replyToken: event.replyToken,
-    messages: [ createTextEcho(text) ],
+    messages: [ createTextEcho(commonText), ...rest ],
   });
 };
