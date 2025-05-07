0cf68c25c8ca80427b068b019620358ab6ccd841
Chih Kai Wen <ckwen1988@gmail.com>
2025-05-07 20:09:45 +0800

    feat: update migration

--- migrations/0002_foamy_the_order.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "display_name" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "picture_url" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "status_message" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "language" text;
